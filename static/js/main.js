07fecc28144e24127942f2adfa9159f36e5b602c
AnjaliRawatAR <anjalivipulrawat@gmail.com>
2025-03-15 17:29:55 +0530

    landing page

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,24 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-link");
-const menuBtnIcon = document.querySelector("i");
-
-menuBtn.addEventListener("click", (e) => {
-    navLinks.classList.toggle("open");
-    const isopen = navLinks.classList.contains("open");menuBtnIcon.setAttribute("class", isopen ? "ri-close-line" : "ri-menu-line");
+// Smooth scrolling for navbar links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
 });
 
-navLinks.addEventListener("click", (e) => {
-    navLinks.classList.remove("open");
-    menuBtnIcon.setAttribute("class", "ri-menu-line");
-})
+// Image Slider Logic
+let slides = document.querySelectorAll('.slide');
+let index = 0;
 
+function showSlides() {
+    slides.forEach(slide => slide.style.display = 'none');
+    index = (index + 1) % slides.length;
+    slides[index].style.display = 'block';
+}
+
+setInterval(showSlides, 2000);
